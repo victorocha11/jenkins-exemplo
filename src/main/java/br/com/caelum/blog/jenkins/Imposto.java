2b611d4b107fee9354674684cfa7073a5e8c9722
victorocha11 <victorocha11@gmail.com>
2016-06-28 13:41:52 -0300

    Update Imposto.java

--- src/main/java/br/com/caelum/blog/jenkins/Imposto.java
+++ src/main/java/br/com/caelum/blog/jenkins/Imposto.java
@@ -4,7 +4,12 @@ public class Imposto {
 
 	public Imposto() {
 	}
-
+	
+	int a,b;
+	a=0;
+	b=0;
+	a=a;
+	
 	public double calcula(Orcamento orcamento) {
 		System.out.println("teste");
 		return orcamento.getValor() * 0.1;
